f46a666f4d1d09af23c9226c0f8b6ced1bce3bba
juliafealves <juliafealves>
2018-04-15 12:21:32 -0300

    Solution of problem: Simple Product.

---
+++ 1004/simple-product.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Author: Júlia Fernandes Alves.
+# Simple Product - Beginner
+
+number_a = int(raw_input())
+number_b = int(raw_input())
+
+product = number_a * number_b
+
+print 'PROD = %i' % product
