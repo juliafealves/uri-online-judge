b93e31c9bdaecf481b5119de6da3210f1e17bed7
juliafealves <juliafealves>
2018-04-15 12:00:56 -0300

    Solution of problem: Area of a Circle.

---
+++ 1002/area-circle.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Author: Júlia Fernandes Alves.
+# Area of a Circle - Beginner
+
+PI = 3.14159
+radius = float(raw_input())
+
+area = PI * radius ** 2
+
+print 'A=%0.4f' % area
