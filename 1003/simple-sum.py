fbf400f30090d58f025b1ad2b6b805656872fac5
juliafealves <juliafealves>
2018-04-15 12:13:24 -0300

    Solution of problem: Simple Sum.

---
+++ 1003/simple-sum.py
@@ -0,0 +1,10 @@
+# -*- coding: utf-8 -*-
+# Author: Júlia Fernandes Alves.
+# Simple Sum - Beginner
+
+number_a = int(raw_input())
+number_b = int(raw_input())
+
+total = number_a + number_b
+
+print 'SOMA = %i' % total
